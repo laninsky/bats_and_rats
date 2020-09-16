733abd2dde2a185c0f67c078b1a9078b74d309d4
Alana Alexander <laninsky@gmail.com>
2020-09-16 21:09:26 +1200

    Update monomorphic_vcf.R

--- fastsimcoal2_inputs/monomorphic_vcf.R
+++ fastsimcoal2_inputs/monomorphic_vcf.R
@@ -14,8 +14,19 @@ invariant_row <- temp[1,]
 # Grabbing the first ref/ref entry to duplicate
 entry_to_duplicate <- as.character(invariant_row[grep("0/0",as.character(invariant_row))[1]])
 
-# Duplicating this
+# Duplicating this (assuming sample starts in 10th column)
 invariant_row[10:length(invariant_row)] <- entry_to_duplicate
 
-# Creating the output
+# Creating an output matrix with the invariant row duplicated for the total number of invariant sites
 output <- matrix(invariant_row,nrow=as.numeric(fastsimcoal_inputs[3,1]),ncol=length(invariant_row),byrow=TRUE)
+
+# Modifying some of the columns to give unique IDs, and to note the loci are monomorphic
+output[,2] <- c(1:dim(output)[1])
+output[,1] <- "Mono"
+output[,3] <- paste(output[,1],output[,2],sep="_")
+
+# Converting temp to matrix
+temp <- as.matrix(temp)
+
+# Binding the invariant rows to the rest of the vcf
+temp <- rbind(temp,output)
