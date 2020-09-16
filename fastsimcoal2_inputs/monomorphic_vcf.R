ba446a6d14dcf4000d0b5c887fba36b345c7eaa7
Alana Alexander <laninsky@gmail.com>
2020-09-16 20:31:33 +1200

    Update monomorphic_vcf.R

--- fastsimcoal2_inputs/monomorphic_vcf.R
+++ fastsimcoal2_inputs/monomorphic_vcf.R
@@ -6,3 +6,13 @@ temp <- read_tsv("temp",col_names=TRUE)
 
 # Reading in fastsimcoal_inputs
 fastsimcoal_inputs <- read_tsv("fastsimcoal_inputs",col_names=FALSE)
+
+# Making an "invariant" row that will be duplicated for the total number of 
+# invariant sites not recorded in the vcf.
+invariant_row <- temp[1,]
+
+# Grabbing the first ref/ref entry to duplicate
+entry_to_duplicate <- as.character(invariant_row[grep("0/0",as.character(invariant_row))[1]])
+
+# Duplicating this
+invariant_row[10:length(invariant_row)] <- entry_to_duplicate
