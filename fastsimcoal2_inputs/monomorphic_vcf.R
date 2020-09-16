c548bba584b3877f4d40b68b407e4972b39c298a
Alana Alexander <laninsky@gmail.com>
2020-09-16 20:06:22 +1200

    Update monomorphic_vcf.R

--- fastsimcoal2_inputs/monomorphic_vcf.R
+++ fastsimcoal2_inputs/monomorphic_vcf.R
@@ -1,1 +1,8 @@
+# Loading libraries
+library(tidyverse)
 
+# Reading in the vcf
+temp <- read_tsv("temp",col_names=TRUE)
+
+# Reading in fastsimcoal_inputs
+fastsimcoal_inputs <- read_tsv("fastsimcoal_inputs",col_names=FALSE)
