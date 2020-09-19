e3110ece5276f03d6f7b3809337c457f8cde6b31
Alana Alexander <laninsky@gmail.com>
2020-09-19 20:04:00 +1200

    Update initial_search_range.R

--- fastsimcoal2_optimising/initial_search_range.R
+++ fastsimcoal2_optimising/initial_search_range.R
@@ -84,7 +84,64 @@ if (!(temp[4]==0 & temp[5]==0)) {
 
 if (create_new_est_file) {
   cat("Some parameter estimates are greater than the upper bound of the initial search range\nor within two orders of magnitude of the lower bound of the initial search range.\nA new est file has been written out to start again\n")
-  
+  # Updating $NPOP1$
+  temp <- unlist(strsplit(temp_est[7]," "))
+  temp[4] <- round(min(likelihoods[,2])/100)
+  temp[5] <- max(likelihoods[,2])
+  temp_est[7] <- paste(temp,collapse=" ")
+
+  # Updating $NPOP2$
+  temp <- unlist(strsplit(temp_est[8]," "))
+  temp[4] <- round(min(likelihoods[,3])/100)
+  temp[5] <- max(likelihoods[,3])
+  temp_est[8] <- paste(temp,collapse=" ")
+
+  # Updating $ANCNPOP1$
+  temp <- unlist(strsplit(temp_est[9]," "))
+  temp[4] <- round(min(likelihoods[,4])/100)
+  temp[5] <- max(likelihoods[,4])
+  temp_est[9] <- paste(temp,collapse=" ")
+
+  # Updating $ANCNPOP2$
+  temp <- unlist(strsplit(temp_est[10]," "))
+  temp[4] <- round(min(likelihoods[,5])/100)
+  temp[5] <- max(likelihoods[,5])
+  temp_est[10] <- paste(temp,collapse=" ")
+
+  # Updating $ANCNPOPTOT$
+  temp <- unlist(strsplit(temp_est[11]," "))
+  temp[4] <- round(min(likelihoods[,6])/100)
+  temp[5] <- max(likelihoods[,6])
+  temp_est[11] <- paste(temp,collapse=" ")
+
+  # Updating $TDIV1$
+  temp <- unlist(strsplit(temp_est[12]," "))
+  temp[4] <- round(min(likelihoods[,7])/100)
+  temp[5] <- max(likelihoods[,7])
+  temp_est[12] <- paste(temp,collapse=" ")
+
+  # Updating $TMIG$
+  temp <- unlist(strsplit(temp_est[13]," "))
+  temp[4] <- round(min(likelihoods[,8])/100)
+  temp[5] <- max(likelihoods[,8])
+  temp_est[13] <- paste(temp,collapse=" ")
+
+  # Updating $MIG$
+  temp <- unlist(strsplit(temp_est[14]," "))
+  if (!(temp[4]==0 & temp[5]==0)) {
+    temp[4] <- round(min(likelihoods[,9])/100)
+    temp[5] <- max(likelihoods[,9])
+    temp_est[14] <- paste(temp,collapse=" ")
+  }
+
+  # Updating $MIG1$
+  temp <- unlist(strsplit(temp_est[15]," "))
+  if (!(temp[4]==0 & temp[5]==0)) {
+    temp[4] <- round(min(likelihoods[,10])/100)
+    temp[5] <- max(likelihoods[,10])
+    temp_est[15] <- paste(temp,collapse=" ")
+  }
+  write_lines(temp_est,paste(est_file_name,".new",sep=""))
 } else {
   cat("No parameter estimate is greater than the upper bound of the initial search range\nor within two orders of magnitude of the lower bound of the initial search range. Good to go!\n")
 }
