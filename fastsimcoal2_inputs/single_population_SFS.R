0588bff8605c16f081918d1743bc7330e0cfa44a
Alana Alexander <laninsky@gmail.com>
2020-09-28 16:19:34 +1300

    Update single_population_SFS.R

--- fastsimcoal2_inputs/single_population_SFS.R
+++ fastsimcoal2_inputs/single_population_SFS.R
@@ -6,7 +6,15 @@ joint_SFS_file <- list.files(pattern="jointMAFpop1_0.obs")
 joint_SFS <- readLines(joint_SFS_file)
 
 # Getting rid of title rows
-joint_SFS <- joint_SFS[-2]
+joint_SFS <- joint_SFS[-c(1,2)]
 
 # Getting rid of row titles
-joint_SFS <- gsub(
+joint_SFS <- gsub(".*\t","",joint_SFS)
+
+# Getting number of entries needed for output SFS
+d0_size <- length(unlist(strsplit(joint_SFS[1],split=" ")))-1
+d1_size <- length(joint_SFS)-1
+SFS_size <- d0_size + d1_size + 1
+
+# Creating output
+SFS_output <- matrix(0,nrow=1,ncol=SFS_size)
