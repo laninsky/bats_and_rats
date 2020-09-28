cd33e2687c95be7bfefa1e9993a9cd27407317e2
Alana Alexander <laninsky@gmail.com>
2020-09-28 16:12:15 +1300

    Update single_population_SFS.R

--- fastsimcoal2_inputs/single_population_SFS.R
+++ fastsimcoal2_inputs/single_population_SFS.R
@@ -1,1 +1,12 @@
+# Loading necessary libraries
+library(tidyverse)
 
+# Reading in the joint SFS
+joint_SFS_file <- list.files(pattern="jointMAFpop1_0.obs")
+joint_SFS <- readLines(joint_SFS_file)
+
+# Getting rid of title rows
+joint_SFS <- joint_SFS[-2]
+
+# Getting rid of row titles
+joint_SFS <- gsub(
