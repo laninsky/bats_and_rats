f289f4b85e90f53330a3bbd89ce6928df0d51a9b
Alana Alexander <laninsky@gmail.com>
2020-09-29 15:33:30 +1300

    Update initial_search_range.R

--- fastsimcoal2_optimising/initial_search_range.R
+++ fastsimcoal2_optimising/initial_search_range.R
@@ -6,11 +6,11 @@ folder_with_most_likely_replicate <- "1"
 library(tidyverse)
 
 # Reading in likelihoods file to summarize min and max
-likelihoods <- read_tsv(paste(original_search_range_folder,"/likelihoods.txt",sep=""),col_names=FALSE)
+likelihoods <- read_tsv(paste(folder_with_most_likely_replicate,"/likelihoods.txt",sep=""),col_names=FALSE)
 
 # Reading in est file to modify
-est_file_name <- list.files(path=folder_with_most_likely_replicate,pattern=".est$")
-temp_est <- readLines(paste(folder_with_most_likely_replicate,"/",est_file_name,sep=""))
+est_file_name <- list.files(path=original_search_range_folder,pattern=".est$")
+temp_est <- readLines(paste(original_search_range_folder,"/",est_file_name,sep=""))
 
 # Creating an object to record whether we need to recreate an est file or not
 create_new_est_file <- FALSE
