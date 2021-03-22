aa51dcc14cae1d5c7eab644acb99257c358dc078
Alana Alexander <laninsky@gmail.com>
2021-03-23 06:02:41 +1300

    Update monomorphic_vcf.R
    Modifying code so monomorphic data doesn't have 100% completeness, but reflects patterns in the rest of the vcf file.

--- fastsimcoal2_inputs/monomorphic_vcf.R
+++ fastsimcoal2_inputs/monomorphic_vcf.R
@@ -1,3 +1,6 @@
+# 0.1: 22Mar21: modified the monomorphic sites added from no missing data to patterns of missing data reflecting the variable sites
+# 0.0: original code
+
 # Loading libraries
 library(tidyverse)
 
@@ -7,19 +10,25 @@ temp <- read_tsv("temp",col_names=TRUE)
 # Reading in fastsimcoal_inputs
 fastsimcoal_inputs <- read_tsv("fastsimcoal_inputs",col_names=FALSE)
 
-# Making an "invariant" row that will be duplicated for the total number of 
-# invariant sites not recorded in the vcf.
-invariant_row <- temp[1,]
+# Creating a temp_monomorphic dataframe consisting of just monomorphic data
+gsub_function_01 <- function(x) (gsub("0/1:","0/0:",x))
+gsub_function_10 <- function(x) (gsub("1/0:","0/0:",x))
+gsub_function_11 <- function(x) (gsub("1/1:","0/0:",x))
 
-# Grabbing the first ref/ref entry to duplicate
-entry_to_duplicate <- as.character(invariant_row[grep("0/0",as.character(invariant_row))[1]])
+temp_monomorphic <- temp %>% mutate_all(gsub_function_01)
+temp_monomorphic <- temp_monomorphic %>% mutate_all(gsub_function_10)
+temp_monomorphic <- temp_monomorphic %>% mutate_all(gsub_function_11)
 
-# Duplicating this (assuming sample starts in 10th column)
-invariant_row[10:length(invariant_row)] <- entry_to_duplicate
+# Figuring out the number of times this temp_monomorphic dataframe
+# needs to be copied in order to have the number of required lines
+number_of_reps_needed <- as.numeric(fastsimcoal_inputs[2,1])/dim(temp)[1]
+full_reps <- round(number_of_reps_needed)
+rest_of_lines <- (number_of_reps_needed - round(number_of_reps_needed))*dim(temp)[1]
 
-# Creating an output matrix with the invariant row duplicated for the total number of invariant sites
-output <- matrix(invariant_row,nrow=as.numeric(fastsimcoal_inputs[2,1]),ncol=length(invariant_row),byrow=TRUE)
-
+output_full_reps <- temp_monomorphic[rep(1:dim(temp_monomorphic)[1],full_reps),]
+output_rest_of_lines <- temp_monomorphic[1:rest_of_lines,]
+output <- rbind(output_full_reps,output_rest_of_lines)
+  
 # Modifying some of the columns to give unique IDs, and to note the loci are monomorphic
 output[,2] <- c(1:dim(output)[1])
 output[,1] <- "Mono"
@@ -33,3 +42,4 @@ temp <- rbind(temp,output)
 
 # Write out the "new" vcf file
 write.table(temp,"temp",col.names=TRUE,sep="\t",row.names=FALSE,quote=FALSE)
+
